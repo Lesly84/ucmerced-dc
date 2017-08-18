ca81815f8969e3d3b9b4b19425b6f446df68ff90
Lesly84 <llopez84@ucmerced.edu>
2017-08-18 10:02:33 -0700

    Provided description of data

--- 2017-08-17_sample-script.R
+++ 2017-08-17_sample-script.R
@@ -5,6 +5,7 @@
 
 
 ##Download data ----
+##Data is surveys from species collected in the field
 #download.file("https://ndownloader.figshare.com/files/2292169",
 #              "data/portal_data_joined.csv")
 
